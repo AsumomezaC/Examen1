e027d15227e07a0e969b158041585ade8f99a7b6
AsumomezaC <cuevassolisadrian@gmail.com>
2023-03-21 16:02:16 -0600

    Menu basico

--- Menu.cs
+++ Menu.cs
@@ -3,12 +3,56 @@ namespace Examen1{
       
     Jugador player = new Jugador();
 
-    public void ShowMenu(){
-      Console.Clear();
-      Console.WriteLine("Seleccione una opcion");
-      Console.WriteLine("1) Apostar");
-      Console.WriteLine("2) Consultar estadisticas");
-      Console.WriteLine("3) Salir");
+    public bool ShowMenu(){
+
+      int opcion = SeleccionarOpcion();
+      return EjecutarOpciones(opcion);
     }
+
+    public int SeleccionarOpcion(){
+      string? opcion;
+
+      do{
+        Console.Clear();
+        Console.WriteLine("Seleccione una opcion");
+        Console.WriteLine("1) Apostar");
+        Console.WriteLine("2) Consultar estadisticas");
+        Console.WriteLine("3) Salir");
+        opcion = Console.ReadLine();
+        if(opcion != "1" && opcion != "2" && opcion != "3"){ // si no se selecciona una opcion valida
+          Console.WriteLine("Opcion invalida, presione para continuar");
+          Console.ReadLine();
+          opcion = null;
+        }
+      } while (opcion == null);
+
+      return Int32.Parse(opcion);
+    }
+
+    public bool EjecutarOpciones(int opcion){
+      switch(opcion){
+        case 1:
+          return Apostar();
+        case 2:
+          return Estadisticas();
+        default: 
+          Salir();
+          return false;
+      }
+    }
+
+    //acciones posibles
+    public bool Apostar(){
+      return true;
+    }
+
+    public bool Estadisticas(){
+      return true;
+    }
+
+    public void Salir(){
+      Console.WriteLine("Hasta luego, que pase un lindo día");
+    }
+    // fin acciones posibles
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿namespace Examen1{
   class Program{
+    static Menu menu = new Menu();
     static void Main(string[] args){
       // presentacion
       Console.WriteLine("Bienvenido al Cazino");
-      
+      do{
+      }while(menu.ShowMenu());
     }
   }
 }
